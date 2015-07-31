0a26ee1d0ee7863bfb846c2afd8197b1fcea35b3
zhoukekestar <zhoukekestar@163.com>
2015-07-31 21:09:23 +0800

    Rename to consoleTest

--- nodejs/test/console.js
+++
@@ -1,8 +0,0 @@
-// test 1
-//require('../lib/console.js')(['console.js'])
-
-// test 2
-require('../lib/console.js')([], ['console.js'])
-
-console.log('hello world, log.');
-console.info('hello world, info.')
---
+++ nodejs/test/consoleTest.js
@@ -0,0 +1,8 @@
+// test 1
+//require('../lib/console.js')(['consoleTest.js'])
+
+// test 2
+require('../lib/console.js')([], ['consoleTest.js'])
+
+console.log('hello world, log.');
+console.info('hello world, info.')
