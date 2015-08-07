88227428241a6391d0d94ba7bab0f61b8f039a8d
zhoukekestar <zhoukekestar@163.com>
2015-08-07 18:03:53 +0800

    Remove wechat config.

--- nodejs/lib/viewEngine.js
+++ nodejs/lib/viewEngine.js
@@ -1,11 +1,12 @@
 /**
- * View engine based on jade.
- * Need render.js
+ * View engine based on :
+ * 1. jade
+ * 2. ejs
+ * 3. render.js
  */
 var fs      = require('fs');
 var ejs     = require('ejs');
 var jade    = require('jade');
-var wechat  = require('./wechat');
 
 module.exports = function(app) {
 
@@ -28,18 +29,17 @@ module.exports = function(app) {
   // JADE
   app.engine('jade', function(filePath, options, callback) {
 
-    if (options._req && options._req.headers['x-requested-with'] === 'XMLHttpRequest') {
-      options._ajax = true
-    } else {
-      options._ajax = false;
-    }
 
     fs.readFile(filePath, function(err, content) {
-      if (err) return callback(new Error(err));
+
+      if (err) {
+        return callback(new Error(err));
+      }
 
       content += '';
+
       // AJAX request, no layout needed.
-      if (options._ajax) {
+      if (options._req && options._req.xhr) {
         content = content.replace('extends', '//- extends')
       }
 
@@ -52,34 +52,11 @@ module.exports = function(app) {
         console.error(e.stack);
         throw new Error('Jade Error')
       }
-      if (html.indexOf('$.shareWXConfig()') !== -1) {
-        var req = options._req;
-        var url = 'http://' + req.headers.host + req.originalUrl;
 
-        wechat.getSign(url, function(time, random, sign) {
-
-          var d = {
-            url: url,
-            debug: false,
-            appId: wechat.appID,  // 必填，公众号的唯一标识
-            timestamp: time,      // 必填，生成签名的时间戳
-            nonceStr: random,     // 必填，生成签名的随机串
-            signature: sign       // 必填，签名，见附录1
-          };
-
-          html = html.replace('$.shareWXConfig()', '$.shareWXConfig(' + JSON.stringify(d) + ')');
-          return callback(null, html);
-
-        });
-
-      } else {
-        return callback(null, html);
-      }
+      return callback(null, html);
 
     });
 
   });
-
-
 
 };
