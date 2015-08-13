38df1938c0011678ddbda45854f69e4f6015c328
zhoukekestar <zhoukekestar@163.com>
2015-08-13 10:48:54 +0800

    Add scss & comment.

--- src/alert/alert.js
+++ src/alert/alert.js
@@ -6,25 +6,31 @@
   }
 }( function() {
 
-  var _alert = window.alert;
-  window.alert = function(msg, sys) {
+  var _alert = window.alert,
+      temp,
+      useSystem = false;
+  window.alert = function(msg) {
 
-    // Default value is false
-    sys = sys === undefined ? false : true;
+    // Use system alert OR not.
+    if (typeof msg === 'boolean') {
+      useSystem = msg;
+      return;
+    }
 
-    if (sys === true) {
+    if (useSystem === true) {
       _alert(msg)
     } else {
 
       var dialog = '<div class="system-alert-wrapper"><div class="system-alert"><div class="content">' + msg + '</div><button class="close" onclick="">确定</button></div></div>';
       //dialog = (new DOMParser()).parseFromString(dialog, "text/xml").firstChild;
 
-      var temp = document.createElement("div")
+      // String to dom.
+      temp = temp === undefined ? document.createElement("div") : temp;
       temp.innerHTML = dialog
-
       dialog = temp.firstChild;
+      temp.innerHTML = '';
 
-      document.getElementsByTagName('body')[0].appendChild(dialog)
+      document.getElementsByTagName('body')[0].appendChild(dialog);
 
       dialog.addEventListener('click', function(e) {
         if (e.target.className === 'close') {
