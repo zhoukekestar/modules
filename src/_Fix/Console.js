05bf42cb4d17841cc8c97daea9e88c730c1e4f3b
zhoukekestar <zhoukekestar@163.com>
2015-08-13 15:28:25 +0800

    add console

---
+++ src/_Fix/Console.js
@@ -0,0 +1,10 @@
+
+// For some IE that have no console object.
+if (!window.console) {
+  window.console = {};
+  window.console.debug  = function(){}
+  window.console.log    = function(){}
+  window.console.info   = function(){}
+  window.console.warn   = function(){}
+  window.console.error  = function(){}
+}
