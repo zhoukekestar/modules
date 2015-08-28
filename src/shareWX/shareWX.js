e97e7ec988c22e6fdaddc478399854a30a874f43
zhoukekestar <zhoukekestar@163.com>
2015-08-28 11:31:06 +0800

    set default debug to false

--- src/shareWX/shareWX.js
+++ src/shareWX/shareWX.js
@@ -18,7 +18,7 @@
      *
      */
     api       : '/wechat/sdk/sign',
-    debug     : true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug     : false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 
     // Url config
     appId     : '', // 必填，公众号的唯一标识
