7dc1074503c527455767662ec3f5047cacec281d
zhoukekestar <zhoukekestar@163.com>
2015-09-08 16:13:34 +0800

    fix bu

--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -9,26 +9,26 @@
 
     var init = function() {
         $("[data-role='tabs']").each(function () {
-            var witdh = 100 / $(this).find('.tabs-list li').length;
-          $(this).find('.tabs-list li').css('width', witdh + '%');
+            var witdh = 100 / $(this).find('.tabs-list > li').length;
+          $(this).find('.tabs-list > li').css('width', witdh + '%');
 
             var $this = $(this);
-            var active_tab = $this.find(".tabs-list li.active").data("href");
+            var active_tab = $this.find(".tabs-list > li.active").data("href");
 
             // No active tab find. First child element set to active.
-            if (active_tab == undefined)
-                active_tab = $(this).find(".tabs-list li").eq(0).data("href");
+            if (active_tab === undefined)
+                active_tab = $(this).find(".tabs-list > li").eq(0).data("href");
 
             $this
-                .find(".tabs-content div").hide().end()
+                .find(".tabs-content > div").hide().end()
                 .find(active_tab).show();
 
-            $this.find(".tabs-list li").click(function () {
+            $this.find(".tabs-list > li").click(function () {
 
                 var active_tab = $(this).data("href");
                 $this
-                    .find(".tabs-list li").removeClass("active").end()
-                    .find(".tabs-content div").hide().end()
+                    .find(".tabs-list > li").removeClass("active").end()
+                    .find(".tabs-content > div").hide().end()
                     .find(active_tab).show();
                 $(this).addClass("active");
             });
