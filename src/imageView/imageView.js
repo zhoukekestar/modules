f8e11485e68df9822cd417540ebff0d77c72aeec
keke <zhoukekestar@163.com>
2015-09-18 14:51:31 +0800

    add imageView

---
+++ src/imageView/imageView.js
@@ -0,0 +1,44 @@
+!( function( factory ) {
+  if ( typeof define === "function" && define.amd ) {
+    define(factory );
+  } else {
+    factory( );
+  }
+}(function( ){
+
+  var imageView = null;
+
+  var initImageView = function() {
+    imageView = document.createElement('div');
+    imageView.classList.add('imageView');
+    imageView.innerHTML = '<span>&times;</span><div class="img"></div>';
+    document.body.appendChild(imageView);
+
+
+    imageView.querySelector('span').addEventListener('click', function() {
+      imageView.style.opacity = 0;
+      setTimeout(function(){
+        imageView.style.zIndex = -1;
+      }, 500)
+    })
+
+    return imageView;
+  }
+
+  document.addEventListener('click', function(e) {
+    var target = e.target;
+
+    if (target.getAttribute('data-role') === 'imageView') {
+      if (!imageView) initImageView();
+
+      imageView.querySelector('.img').style.backgroundImage = 'url(' + target.getAttribute('src') + ')';
+      imageView.style.opacity = 1;
+      imageView.style.zIndex = 200;
+    }
+  })
+
+
+  return null;
+
+
+}));
