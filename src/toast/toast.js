6cab5364fccf944c4cc3512e09d7a6d66983a469
keke <zhoukekestar@163.com>
2015-10-29 18:57:38 +0800

    Separate style form js.

--- src/toast/toast.js
+++ src/toast/toast.js
@@ -11,15 +11,17 @@
    * @param  {[string, object]} options
    * @return {[object]}         [callback function]
    */
+  var div = null;
 	var alertMsg = function( options ) {
 
-    var defaults = {
-      width     : 160,
-      content   : "?",
-      done      : null,
-      time      : 1200,
-      autohide  : true
-    };
+    var callback
+      , defaults = {
+          width     : 160,
+          content   : "?",
+          done      : null,
+          time      : 1200,
+          autohide  : true
+        };
 
     // Build options
     if ( typeof( options ) === "string" ) {
@@ -35,74 +37,38 @@
       options = defaults;
     }
 
-    var
-      // callback function
-      callback,
-
-      alertMsgCSS = {
-        "color": "#FFF",
-        "font-size": "18px",
-        "background-color": "rgba(0, 0, 0, 0.8)",
-        "border-radius": "10px",
-        "box-sizing": "border-box",
-        "text-align": "center",
-        "z-index": "99999",
-        "word-break": "break-word",
-        "padding": "12px",
-        "position": "fixed",
-        "left": "50%",
-        "top": "50%",
-        "text-shadow": "none",
-        "width": options.width + 'px',
-        "margin-left": '-' + ( options.width / 2 ) + 'px',
-        "opacity": 0,
-        "-webkit-transition": "all 1s",
-        "transition": "all 1s"
-      },
-
-      div = document.createElement('div'),
-
-      css,
-
-      needRemove = false;
-
-
-
-    // Build alertMsg's Div element.
-    div.innerHTML = options.content;
-    div.classList.add('toast');
-
-    for (css in alertMsgCSS) {
-      div.style[css] = alertMsgCSS[css];
+    if (!div) {
+      div = document.createElement('div');
+      div.classList.add('toast');
+      document.body.appendChild(div);
     }
 
-    // Before compute div's height, you should put it into current document.
-    document.body.appendChild(div);
-    div.style.marginTop = '-' + (window.getComputedStyle(div).height.replace(/px/, '') / 2) + 'px';
+    div.innerHTML = options.content;
+    div.style.display = 'block';
 
+    setTimeout(function(){
+      div.style.marginTop = '-' + (window.getComputedStyle(div).height.replace(/px/, '') / 2) + 'px';
+      div.style.opacity = "1";
+    }, 10)
 
-    div.style.opacity = "1";
 		if ( options.autohide ) {
 
 			setTimeout( function() {
-        div.style.opacity = "0";
-        needRemove = true;
+
+        div.style.opacity = 0;
+
+        // After anmiation.
+        setTimeout(function(){
+
+          div.style.cssText = ''
+
+          typeof callback === 'function' && callback();
+          typeof options.done === 'function' && options.done();
+        }, 1000)
+
 			}, options.time );
 
 		}
-
-    // Listen transition
-    div.addEventListener((div.style.transition === undefined ? 'webkitTransitionEnd' : 'transitionend'), function(){
-      if (needRemove === true) {
-        div.remove();
-        if (typeof callback === 'function') {
-         callback();
-        }
-        if (typeof options.done === 'function') {
-          options.done();
-        }
-      }
-    });
 
     return {
       then: function(cb){
