5a89f0c8dd9cb8bb0d55f158fdae0a881963b014
keke <zhoukekestar@163.com>
2015-10-29 18:54:13 +0800

    add delay support.

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ var app         = express();
 
 app.use(express.static(path.join(__dirname, 'src')));
 
+
+app.all('/delay', function (req, res) {
+  setTimeout(function(){
+    res.json({})
+
+  }, (+req.query.time) * 1000)
+})
 app.listen(3000);
 console.log('listen on 3000.')
