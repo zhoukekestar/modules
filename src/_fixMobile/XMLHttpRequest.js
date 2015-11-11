44d59867a79c5bd0d0f6132482b4503c743a1696
keke <zhoukekestar@163.com>
2015-11-11 15:48:36 +0800

    Polyfill XHR sendAsBinary method.

--- src/_fixMobile/XMLHttpRequest.js
+++ src/_fixMobile/XMLHttpRequest.js
@@ -118,4 +118,25 @@
     };
   }
 
+
+  /*\
+  |*|
+  |*|  :: XMLHttpRequest.prototype.sendAsBinary() Polyfill ::
+  |*|
+  |*|  https://developer.mozilla.org/en-US/docs/DOM/XMLHttpRequest#sendAsBinary()
+  \*/
+
+  if (!XMLHttpRequest.prototype.sendAsBinary) {
+    XMLHttpRequest.prototype.sendAsBinary = function(sData) {
+      var nBytes = sData.length, ui8Data = new Uint8Array(nBytes);
+      for (var nIdx = 0; nIdx < nBytes; nIdx++) {
+        ui8Data[nIdx] = sData.charCodeAt(nIdx) & 0xff;
+      }
+      /* send as ArrayBufferView...: */
+      // this.send(ui8Data);
+      this.send(ui8Data.buffer);
+      /* ...or as ArrayBuffer (legacy)...: this.send(ui8Data.buffer); */
+    };
+  }
+
 })(this, this.XMLHttpRequest);
