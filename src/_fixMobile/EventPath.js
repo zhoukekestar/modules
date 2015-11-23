21c959f7dd5e1f1277debc07b8edfe0564b40e3f
keke <zhoukekestar@163.com>
2015-11-23 09:20:43 +0800

    Bugfix: Can't fill path when trigger event except CLICK.

--- src/_fixMobile/EventPath.js
+++ src/_fixMobile/EventPath.js
@@ -1,10 +1,11 @@
-(function(global){
+!(function(global){
 
   // For Android 4.3- (included)
   var pathFill = function() {
     var e = arguments[0];
 
     if (!e.path) {
+
       e.path = [];
       var t = e.target;
       while (t !== document) {
@@ -16,8 +17,13 @@
     }
   }
 
-  document.body.addEventListener('click', pathFill);
-  document.body.addEventListener('click', pathFill, true);
+  var events = ['click', 'taphold', 'swipeleft', 'swiperight'];
+  events.forEach(function(event) {
+
+    document.body.addEventListener(event, pathFill)
+    document.body.addEventListener(event, pathFill, true)
+
+  })
 
 })(window);
 
