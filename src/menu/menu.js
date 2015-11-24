0ad19588afd30d696fc76363ebad1876857d721f
keke <zhoukekestar@163.com>
2015-11-24 19:35:23 +0800

    Make sure that menu module run only once.

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -6,58 +6,59 @@
   }
 }( function() {
 
+  var addListener = function() {
 
-  document.addEventListener('click', function(e) {
+    document.addEventListener('click', function(e) {
 
-
-
-    var path = e.path && e.path.concat();
-    if (path[0].nodeName && path[0].nodeName !== 'A' && path[1].nodeName && path[1].nodeName !== 'A' && path[2].nodeName && path[2].nodeName !== 'A') {
-      return;
-    }
-
-    while (path[0].nodeName && path[0].nodeName !== 'A') {
-      path.shift();
-    }
-    var target = path[0];
-
-    // ul > li > a
-    if (target.nodeName === 'A' && path[1].nodeName === 'LI' && path[2].nodeName === 'UL'){
-
-      // ul.nav > li > a
-      if (path[2].classList.contains('nav')) {
-
-        var mainNav = target;
-        var subNav = mainNav.nextElementSibling;
-
-        // ul.nav > li > a + .sub-nav
-        if (subNav && subNav.classList.contains('sub-nav')) {
-          e.preventDefault();
-
-          if (subNav.classList.contains('collapse'))
-            subNav.classList.remove('collapse')
-          else
-            subNav.classList.add('collapse');
-
-        // Save current menu
-        } else {
-          sessionStorage['menu'] = mainNav.getAttribute('data-menu') || mainNav.getAttribute('href')  || new Date().getTime();
-        }
-
-      // ul.sub-nav > li > a
-      } else if (path[2].classList.contains('sub-nav')) {
-
-        var mainNav = target;
-        var subNav = mainNav.nextElementSibling;
-
-        // No any more sub-nav
-        if (!subNav) {
-          sessionStorage['menu'] = mainNav.getAttribute('data-menu') || mainNav.getAttribute('href') || new Date().getTime();
-        }
-
+      var path = e.path && e.path.concat();
+      if (path[0].nodeName && path[0].nodeName !== 'A' && path[1].nodeName && path[1].nodeName !== 'A' && path[2].nodeName && path[2].nodeName !== 'A') {
+        return;
       }
-    }
-  })
+
+      while (path[0].nodeName && path[0].nodeName !== 'A') {
+        path.shift();
+      }
+      var target = path[0];
+
+      // ul > li > a
+      if (target.nodeName === 'A' && path[1].nodeName === 'LI' && path[2].nodeName === 'UL'){
+
+        // ul.nav > li > a
+        if (path[2].classList.contains('nav')) {
+
+          var mainNav = target;
+          var subNav = mainNav.nextElementSibling;
+
+          // ul.nav > li > a + .sub-nav
+          if (subNav && subNav.classList.contains('sub-nav')) {
+            e.preventDefault();
+
+            if (subNav.classList.contains('collapse'))
+              subNav.classList.remove('collapse')
+            else
+              subNav.classList.add('collapse');
+
+          // Save current menu
+          } else {
+            sessionStorage['menu'] = mainNav.getAttribute('data-menu') || mainNav.getAttribute('href')  || new Date().getTime();
+          }
+
+        // ul.sub-nav > li > a
+        } else if (path[2].classList.contains('sub-nav')) {
+
+          var mainNav = target;
+          var subNav = mainNav.nextElementSibling;
+
+          // No any more sub-nav
+          if (!subNav) {
+            sessionStorage['menu'] = mainNav.getAttribute('data-menu') || mainNav.getAttribute('href') || new Date().getTime();
+          }
+
+        }
+      }
+    })
+
+  }
 
   var init = function() {
     var role = document.querySelector('[data-role="menu"]'),
@@ -66,7 +67,9 @@
         max;
 
     if (role && !role[namespace + 'inited']) {
+
       role[namespace + 'inited'] = true;
+      addListener();
 
       var links = role.querySelectorAll('a');
 
