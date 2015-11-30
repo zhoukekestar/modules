c89226efb7c8d4574738d3bd2bd5ad4395627c03
keke <zhoukekestar@163.com>
2015-11-30 14:16:39 +0800

    Add: Lazyload background-image.

--- src/lazyload/lazyload.js
+++ src/lazyload/lazyload.js
@@ -17,16 +17,25 @@
 }(function( ){
 
   // All images.
-  var images = [];
+  var images            = []
+    , defaultImg        = 'data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYGBgAAAABQABh6FO1AAAAABJRU5ErkJggg=='
+    , backgroundImages  = [];
+
   function loadImage (el, fn) {
     var img = new Image()
       , src = el.getAttribute('data-src');
     img.onload = function() {
-      if (!! el.parent)
-        el.parent.replaceChild(img, el)
-      else
-        el.src = src;
+      el.src = src;
+      fn? fn.call(el) : null;
+    }
+    img.src = src;
+  }
 
+  function loadBackgroundImage (el, fn) {
+    var img = new Image()
+      , src = el.getAttribute('data-backgroundimage');
+    img.onload = function() {
+      el.style.backgroundImage = 'url(' + src + ')';
       fn? fn.call(el) : null;
     }
     img.src = src;
@@ -48,31 +57,51 @@
       if (!images[i].lazyloaded && elementInViewport(images[i]) ) {
         loadImage(images[i], function () {
           this.lazyloaded = true;
+          this.classList.remove('lazy');
+        });
+      }
+    };
+
+    for (var i = 0; i < backgroundImages.length; i++) {
+      if (!backgroundImages[i].lazyloaded && elementInViewport(backgroundImages[i]) ) {
+        loadBackgroundImage(backgroundImages[i], function () {
+          this.lazyloaded = true;
+          this.classList.remove('lazy');
         });
       }
     };
   }
 
-  var init = function() {
+  var init = function(reload) {
 
-    images = document.querySelectorAll('img.lazy')
-
+    // For: <img class='lazy' data-src='http://abc.com'>
+    images      = Array.prototype.slice.apply(document.querySelectorAll('img.lazy'))
     for (var i = 0; i < images.length; i++)
-      images[i].src = images[i].src || "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYGBgAAAABQABh6FO1AAAAABJRU5ErkJggg==";
+      images[i].src = images[i].src || defaultImg;
+
+    // For: <img class='lazy delay' src='http://abc.com'>
+    var imagesDelay = document.querySelectorAll('img.lazy.delay');
+    for (var i = 0; i < imagesDelay.length; i++) {
+      imagesDelay[i].setAttribute('data-src', imagesDelay[i].src)
+      imagesDelay[i].src = defaultImg;
+      images.push(imagesDelay[i])
+    }
+
+    // For: <div class='lazy' data-background-image=''>
+    backgroundImages = Array.prototype.slice.apply(document.querySelectorAll('.lazy[data-backgroundimage]'))
+    for (var i = 0; i < backgroundImages.length; i++)
+      backgroundImages[i].style.backgroundImage = 'url(' + (backgroundImages[i].style.backgroundImage || defaultImg) + ')';
 
     processScroll();
-    window.addEventListener('scroll',processScroll);
+
+    if (!reload)
+      window.addEventListener('scroll',processScroll);
 
   }
 
   // For dynamic dom. When inerst html to dom.
   document.addEventListener('lazyload-reload', function() {
-    images = document.querySelectorAll('img.lazy');
-
-    for (var i = 0; i < images.length; i++)
-      images[i].src = images[i].src || "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYGBgAAAABQABh6FO1AAAAABJRU5ErkJggg==";
-
-    processScroll();
+    init(true)
   })
 
 
