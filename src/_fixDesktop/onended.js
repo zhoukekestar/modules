274df6abe8e328c6782ccba594c7a6da0c0c537e
keke <zhoukekestar@163.com>
2015-12-08 11:29:19 +0800

    Bugfixed: 360 7.1 not support onended property.

---
+++ src/_fixDesktop/onended.js
@@ -0,0 +1,22 @@
+!(function () {
+
+  var init = function () {
+    var hasOnendedElements = document.querySelectorAll('[onended]')
+    for (var i = 0; i < hasOnendedElements.length; i++) {
+      if (!hasOnendedElements[i].onended) {
+        hasOnendedElements[i].onended = new Function(hasOnendedElements[i].getAttribute('onended'));
+      }
+    }
+  }
+
+  if (document.readyState === 'complete' || document.readyState === 'interactive') {
+   init()
+  } else {
+   document.addEventListener('readystatechange', function(e) {
+     if (document.readyState === 'interactive') {
+       init();
+     }
+   })
+  }
+
+})();
