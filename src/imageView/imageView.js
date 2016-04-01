c17018bbcc454cdab8a92eab7272974630ed176d
keke <zhoukekestar@163.com>
2016-04-01 10:54:46 +0800

    Add Feature: preview pre & next imageView-image.

--- src/imageView/imageView.js
+++ src/imageView/imageView.js
@@ -7,22 +7,56 @@
 }(function( ){
 
   var imageView = null;
+  var allImages = null;
+  var currentTarget = null;
 
   var initImageView = function() {
     imageView = document.createElement('div');
     imageView.classList.add('imageView');
-    imageView.innerHTML = '<span>&times;</span><div class="img"></div>';
+    imageView.innerHTML = '<span class="close">&times;</span><div class="img"><span class="pre"><</span><span class="next">></span></div>';
     document.body.appendChild(imageView);
 
 
-    imageView.querySelector('span').addEventListener('click', function() {
+    imageView.querySelector('span.close').onclick = function() {
       imageView.style.opacity = 0;
       setTimeout(function(){
         imageView.style.zIndex = -1;
       }, 500)
-    })
+    }
+
+    imageView.querySelector('span.pre').onclick = function() {
+      for (var i = allImages.length; i >= 0; i--) {
+        if (allImages[i] === currentTarget) {
+          currentTarget = allImages[(i - 1 >= 0 ? i - 1 : allImages.length - 1)];
+          return setImage(currentTarget)
+        }
+      }
+    }
+
+    imageView.querySelector('span.next').onclick = function() {
+      var length = allImages.length;
+
+      for (var i = 0; i < length; i++) {
+        if (allImages[i] === currentTarget) {
+          currentTarget = allImages[(i + 1 < length ? i + 1 : 0)];
+          return setImage(currentTarget)
+        }
+      }
+    }
 
     return imageView;
+  }
+
+  var setImage = function(target) {
+    if (target.getAttribute('src')) {
+      imageView.querySelector('.img').style.backgroundImage = 'url(' + target.getAttribute('src') + ')';
+    } else if(target.style.backgroundImage) {
+      imageView.querySelector('.img').style.backgroundImage = target.style.backgroundImage;
+    } else {
+      imageView.querySelector('.img').style.backgroundImage = 'url(none)';
+    }
+    imageView.style.opacity = 1;
+    imageView.style.zIndex = 1001;
   }
 
   document.addEventListener('click', function(e) {
@@ -31,15 +65,10 @@
     if (target.getAttribute('data-role') === 'imageView') {
       if (!imageView) initImageView();
 
-      if (target.getAttribute('src')) {
-        imageView.querySelector('.img').style.backgroundImage = 'url(' + target.getAttribute('src') + ')';
-      } else if(target.style.backgroundImage) {
-        imageView.querySelector('.img').style.backgroundImage = target.style.backgroundImage;
-      } else {
-        imageView.querySelector('.img').style.backgroundImage = 'url(none)';
-      }
-      imageView.style.opacity = 1;
-      imageView.style.zIndex = 1001;
+      allImages = document.querySelectorAll('[data-role="imageView"]');
+      currentTarget = target;
+
+      setImage(target);
     }
   })
 
